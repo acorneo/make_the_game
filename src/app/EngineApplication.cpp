8ffdff05790ca5c487601efc90b7d8e0fe82d5a2
maksim <maksimraklo846@gmail.com>
2025-06-21 18:19:48 +0300

    Created lines; looks cool

--- src/app/EngineApplication.cpp
+++ src/app/EngineApplication.cpp
@@ -3,6 +3,60 @@
 
 #include <iostream>
 
+// this namespace contains all the positions for engine UI elements
+namespace eui {
+    UI::Line fdivline_global;
+    UI::Line sdivline_global;
+    UI::Line upline;
+    UI::Line downline;
+    UI::Line leftline;
+    UI::Line rightline;
+};
+// in case if window was resized, because it's adaptive interface
+// I have to recalculate all ui elements positions
+void EngineApplication::recalculate() {
+    lines_pipeline.clear();
+    buttons_pipeline.clear();
+
+    float fdivline_global_xstps = 0.2f * static_cast<float>(GetScreenWidth());
+    eui::fdivline_global.xStartPos = static_cast<int>(fdivline_global_xstps);
+    eui::fdivline_global.xEndPos = eui::fdivline_global.xStartPos;
+    eui::fdivline_global.yStartPos = 0;
+    eui::fdivline_global.yEndPos = GetScreenHeight();
+    lines_pipeline.push_back(eui::fdivline_global);
+
+    float sdivline_global_xstps = 0.8f * static_cast<float>(GetScreenWidth());
+    eui::sdivline_global.xStartPos = static_cast<int>(sdivline_global_xstps);
+    eui::sdivline_global.xEndPos = eui::sdivline_global.xStartPos;
+    eui::sdivline_global.yStartPos = 0;
+    eui::sdivline_global.yEndPos = GetScreenHeight();
+    lines_pipeline.push_back(eui::sdivline_global);
+
+    eui::upline.xStartPos = 0;
+    eui::upline.xEndPos = GetScreenWidth();
+    eui::upline.yStartPos = 1;
+    eui::upline.yEndPos = 1;
+    lines_pipeline.push_back(eui::upline);
+
+    eui::downline.xStartPos = 0;
+    eui::downline.xEndPos = GetScreenWidth();
+    eui::downline.yStartPos = GetScreenHeight()-2;
+    eui::downline.yEndPos = GetScreenHeight()-2;
+    lines_pipeline.push_back(eui::downline);
+
+    eui::leftline.xStartPos = 2;
+    eui::leftline.xEndPos = 2;
+    eui::leftline.yStartPos = 0;
+    eui::leftline.yEndPos = GetScreenHeight();
+    lines_pipeline.push_back(eui::leftline);
+
+    eui::rightline.xStartPos = GetScreenWidth()-1;
+    eui::rightline.xEndPos = GetScreenWidth()-1;
+    eui::rightline.yStartPos = 0;
+    eui::rightline.yEndPos = GetScreenHeight();
+    lines_pipeline.push_back(eui::rightline);
+}
+
 void EngineApplication::init() {
     SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 
@@ -17,17 +71,17 @@ void EngineApplication::init() {
         std::cerr << "Provided font is not available" << std::endl;
     }
 
-    /* Just to test */
-    UI::Line line{
-        .xStartPos = 0, .yStartPos = 0, .xEndPos = 200, .yEndPos = 200,
-    };
-    lines_pipeline.push_back(line);
+    recalculate();
 }
 
 void EngineApplication::process() {
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
+        if (IsWindowResized()) {
+            recalculate();
+        }
 
         // 5 layers overall
         for (int i = 0; i < 5; i++) {
--- src/app/EngineApplication.hpp
+++ src/app/EngineApplication.hpp
@@ -8,13 +8,15 @@
 
 class EngineApplication {
     // Those vectors are stuff that has to be rendered; normal people call it render pipeline
+    Font defaultFont = {};
     std::vector<UI::Button> buttons_pipeline;
     std::vector<UI::Line> lines_pipeline;
 
-    Font defaultFont = {};
 public:
     void renderButtons(int zIdx);
     void renderLines(int zIdx);
+
+    void recalculate(); // it is totally not supposed to be inside this class, but otherwise I can't get access to pipelines
 
     void init();
     void process();
